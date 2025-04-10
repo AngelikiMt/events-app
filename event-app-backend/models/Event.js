5f6aee4ea938539aa133ee122ab4628695cdabb2
AngelikiMt <matta.d.angeliki@gmail.com>
2025-04-10 09:59:33 +0300

    Update Event.js

--- event-app-backend/models/Event.js
+++ event-app-backend/models/Event.js
@@ -5,8 +5,12 @@ const mongoose = require('mongoose');
 const eventSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: String,
+    city: String,
+    category: Category,
+    imageUrl: String,
     date: { type: Date, required: true},
     location: String,
+    viewCount: Int = 0,
     createdAt: { type: Date, default: Date.now }
 });
 
